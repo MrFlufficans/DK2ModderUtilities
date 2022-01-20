7847ed9cd973e6682f647459da49c04b41a0159a
MrFlufficans <dc.fluffy.cat@gmail.com>
2022-01-20 18:00:06 +0800

    v0.3 of ModInstaller

--- ModInstallerMaster.ps1
+++ ModInstallerMaster.ps1
@@ -1,4 +1,4 @@
-$Version = 0.1
+$Version = 0.3
 $Update = 0
 $ConfirmUpdate = $null
 $Things = Invoke-RestMethod https://raw.githubusercontent.com/MrFlufficans/DK2ModderUtilities/master/UtilVersion
@@ -7,12 +7,12 @@ $VersionLine = $VersionLine.ToString()
 $VersionMaster = $VersionLine.SubString(($VersionLine.Length) -3)
 
 If (Test-Path -Path ./ModInstallerMaster.ps1 -PathType Leaf) {
-    rm ./ModInstaller.ps1
+    rm ./ModInstallerMaster.ps1
     Invoke-RestMethod https://raw.githubusercontent.com/MrFlufficans/DK2ModderUtilities/master/ModInstallerMaster.ps1 >> ModInstaller.ps1
 }
 
 (Invoke-RestMethod "https://artii.herokuapp.com/make?text=Fluffy+Utils&font=smslant") | Write-Host 
-Write-Host "`n  You Are Running Mod Installer v$Version"
+Write-Host "`n   You Are Running Mod Installer v$Version"
 Write-Host "   Hosted On Github.com/MrFlufficans"
 if ($VersionMaster -gt $Version) {
     Write-Host "`n  There is a New Version Available v$VersionMaster`n        Would you Like to Update?"
@@ -42,3 +42,19 @@ If ($Update) {
     Start-Process powershell ./ModInstaller.ps1
     Exit
 } else {}
+
+if (!(Test-Path -Path .\README.txt)) {
+    Write-Host "`n`n    I couldn't find A ReadMe File"
+    Write-Host "       Please Create one and"
+    Write-Host " Put The Mods Name on the First Line"
+    Start-Sleep 6
+    exit
+}
+$ReadMe = Get-Content .\ReadMe.txt
+$ModName = ($ReadMe).split([Environment]::NewLine) | Select -First 1
+
+if (Test-Path -Path $env:LOCALAPPDATA\KillHouseGames\DoorKickers2\mods\$ModName) {rm $env:LOCALAPPDATA\KillHouseGames\DoorKickers2\mods\$ModName -Recurse}
+Copy-Item -path .\$ModName -Destination $env:LOCALAPPDATA\KillHouseGames\DoorKickers2\mods -Recurse
+
+Write-Host "`n $ReadMe has been Installed"
+Start-Sleep 3
--- README.md
+++ README.md
@@ -6,6 +6,16 @@ This Tool is used to check for Modified Files after an Update.
 
 1.  To use it place the Script into the Game Directories Data Folder, on the end and Run the Script.
 2.  If there is a New version it will prompt you to Update a Simple Yes or Y will suffice, You can Decline but I would suggest Updating.
-3. If you let it update itself It will Automatically Replace itself and Relaunch.
+3.  If you let it update itself It will Automatically Replace itself and Relaunch.
 4.  It will Prompt you for How many Days back you want to compare to, e.g 5 will give you a list of everything Modified from then to the moment you press Enter.
 5.  After displaying the List it will Output it to a Text file Labelled Results.txt in the same Folder the Script is in.
+
+
+# Mod Installer
+This is an Installer that can be placed Alongside Mods and used to Automatically Install them
+Hopefully I can make it super easy for Mod Makers to use so people don't need to install them Manually Anymore
+
+1.  It's Fairly Simple to use this Tool, Just Place it Side by Side with your Mod's Folder.
+2.  Make sure the Top Line of your Readme.txt has the Mods Name.
+3.  All people will need to Do is run the ModInstaller with Powershell and it will Do everything Else.
+4.  The tool can also Update automatically but I am Not sure what I could change.
